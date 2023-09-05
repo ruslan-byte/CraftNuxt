68bed32355c30d75d4904ee0fef44db18cc63625
ruslan-byte <rbaroev0@gmail.com>
2023-09-05 21:19:26 +0300

    Updates

---
+++ assets/login.f24991d3.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./Sprite.7df87a1a.js";import{_ as n}from"./_plugin-vue_export-helper.c27b6911.js";import{c as r,I as c,b as s,o as _}from"./entry.0fe3983a.js";const a={};function l(o,m){const e=t;return _(),r("div",null,[c(o.$slots,"default"),s(e)])}const d=n(a,[["render",l]]);export{d as default};
---
+++ assets/logo.5369b065.js
@@ -0,0 +1,1 @@
+import"./entry.0fe3983a.js";const o=""+globalThis.__publicAssetsURL("images/logo.svg");export{o as _};
